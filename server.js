1d5e233029e1afc603cc64f5f0d4434d34c4139c
Peter Rydell <peterydell@gmail.com>
2020-01-16 21:19:48 -0800

    sidebar

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-$('.ui.left.sidebar').sidebar({
-    dimPage: false,
-    transition: 'push',
-    exclusive: false,
-    closable: false
-    });
+semantic.sidebar.ready = function() {
+    $('.ui.sidebar')
+    .find('.ui.button.sidebar')
+      .on('click', function() {
+        var
+          transition = $(this).data('transition')
+        ;
+        $('ui.sidebar.left')
+          .not('.styled')
+          .sidebar('setting', {
+            transition       : transition,
+            mobileTransition : transition
+          })
+        ;
+        $('ui.sidebar.left').not('.styled').sidebar('toggle');
+      });
+}
+
+$(document)
+  .ready(semantic.sidebar.ready)
+;
