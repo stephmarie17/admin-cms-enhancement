5e470e558477ea1de3b89f5ad0fc2cb6ddcb7a5f
Alexandra Luna <alexandra.michelle.j@gmail.com>
2020-01-22 16:54:18 -0800

    added authentification so that it can be deployed to heroku

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const fs            = require("file-system");
 const PORT          = process.env.PORT || 8080;
 const app           = express();
 
+app.use((req, res, next) => {
+  const auth = { login: 'mammoth', password: 'y@rnAdm1n_' };
+  // parse login and password from headers
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+  const [login, password] = new Buffer(b64auth, 'base64').toString().split(':');
+  // Verify login and password are set and correct
+  if (login && password && login === auth.login && password === auth.password) {
+    // Access granted...
+    return next();
+  }
+  // Access denied...
+  res.set('WWW-Authenticate', 'Basic realm="401"'); // change this
+  res.status(401).send('Authentication required.'); // custom message
+});
+
 app.use(
     express.urlencoded({
       extended: true
