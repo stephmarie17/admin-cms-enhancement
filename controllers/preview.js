db601c27c9be83cb42cf2cb04e7164d45de1f575
Stephanie A <stephanie.m.aurelio@gmail.com>
2020-01-23 19:23:10 -0800

    delete console logs and empty files

--- controllers/preview.js
+++ controllers/preview.js
@@ -6,15 +6,14 @@ const request = require("request");
 // from API get request 
 let data;
 
-
+// Get data from CSV import to compare to API results
 router.post("/api/ChatStories/episode-csv-import", function(req,res) {
     const csvData = req.body;
-    console.log("posting from controller");
     // console.log(req.body);
     let characters = csvData.characters;
 
 
-//connection to API
+//Connection to API
 const URL =
 'https://live-api-stage.yarnapp.co/api/ChatStories?filter=%7B%22where%22:%7B%22id%22:2%7D,%22include%22:%22chatStoryPeople%22%7D';
 
@@ -36,37 +35,30 @@ request({
 });
 
 
-//dummy data returned from csv file
-// let characters = ["Khloe", "Kim", "Kylie"];
-//array that will hold the styling information for each character selected in the csv
+//Array that will hold the styling information for each character selected in the csv
 let characterStyles = [];
-
+// Array that will hold any characters in the CSV that do not exist yet in the ChatStory
 let missingChar = [];
 
-//used for getCharacterStyles()
+//Used for getCharacterStyles()
 const CharacterStyle = require("../lib/characterStyle.js");
 const SystemStyle = require("../lib/systemStyle.js");
 
 
-
+// Gets character data from database
 function getCharacterStyles() {
-    console.log("getCharacterStyles function is working!");
-    
-    //loops through characters in the array containing the csv characters and compares it to characters in database
+    //Loops through characters in the array containing the csv characters and compares it to characters in database
     for (let i = 0; i< characters.length; i++ ) {
        const compC = data[0].chatStoryPeople.find(function(x){return x.title === characters[i]});
 
        if (compC === undefined && characters[i] !== "System" && characters[i] !== "") {
             missingChar.push(characters[i]);
-            console.log("missing characters in controller!");
-            console.log (missingChar);
        } else if (compC) {
             const {title, nameColor, bubbleColor, textColor, alignment} = compC;
 
             //only pushes up the new character to the characterStyles array when the csv character matches the database character
             characterStyles.push( new CharacterStyle (title, nameColor, bubbleColor, textColor, alignment));
         } else if (characters[i] === "System") {
-            console.log ("don't match");
             // console.log(characters[i], data[0].chatStoryPeople[i].title);
             const fgColor = data[0].fgColor;
             characterStyles.push(new SystemStyle ("", fgColor));
